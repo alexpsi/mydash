435dc74cab8cbf756f3380b45712d22a5d877d49
alexpsi <alexpsi256@gmai.com>
2018-07-26 01:54:53 +0200

    More structur

--- keywords.js
+++ keywords.js
@@ -1,4 +1,4 @@
-const { monitor, compose, probe, map } = require('./lib/various');
+const { monitor, compose, probe, map } = require('./lib/mydash');
 
 const sentences = [
     'A meteorologist predicts that it will rain today.',
@@ -15,11 +15,11 @@ const applyCutoff = (cutoff = Infinity) => x => x.filter(word => word.length > c
 const removeStopWords = (stopWords = []) => x => x.filter(word => !stopWords.includes(word));
 
 const pipeline = compose(
-    removeStopWords(STOP_WORDS),
-    applyCutoff(3),
-    tokenize,
-    x => x.toLowerCase(),
-    removePunctuation
+	removeStopWords(STOP_WORDS),
+	applyCutoff(3),
+	tokenize,
+	x => x.toLowerCase(),
+	removePunctuation
 )
 
 // monitor(map(pipeline))(sentences);
@@ -27,22 +27,21 @@ const pipeline = compose(
 const createReverseHash = sentences => sentences.map(pipeline).reduce((acc, doc, docIndex) => {
 	doc.map(word => {
 		acc[word] = acc[word] ? acc[word].concat(docIndex) : [docIndex];
-	})
+	});
 	return acc;
 }, {});
 
 const sortHashBy = comparator => hash => Object.keys(hash).map(key => ({
-    docs: hash[key],
-    count: hash[key].length || 0,
-    key,
+	docs: hash[key],
+	count: hash[key].length || 0,
+	key,
 })).sort(comparator);
 
 const docCount = (a, b) => a.count == b.count ? 0 : a.count > b.count ? 1 : -1;
 
 const orderedReverseIndex = compose(
-    sortHashBy(docCount),
-    createReverseHash
-);
+	sortHashBy(docCount),
+	createReverseHash
+); 
 
-	
 monitor(orderedReverseIndex)(sentences);
